d1e3e520c586ce38e39d122917e2fb04a37efe18
SimonPoe94 <s669073@sbuniv.edu>
2017-02-22 18:47:14 -0600

    Add files via upload

---
+++ appController.js
@@ -0,0 +1,31 @@
+var app = angular.module('myApp', []);
+app.controller('personCtrl', function($scope) {
+    $scope.contactID = "";
+	$scope.orgID = "";
+	$scope.role = "";
+	$scope.firstName = "Alexander";
+	$scope.midName = "Simon";
+    $scope.lastName = "Poe";
+	$scope.nameTitle = "";
+	$scope.nameSufx = "";
+	$scope.addLine1 = "";
+	$scope.addLine2 = "";
+	$scope.addCity = "";
+	$scope.addState = "";
+	$scope.addPost = "";
+	$scope.addCountry = "";
+	$scope.phoneOff = "";
+	$scope.phoneMobl = "";
+	$scope.phoneHome = "";
+	$scope.email = "";
+	$scope.emailAlt = "";
+	
+	//$scope.fullName = function() {
+    //   	return $scope.firstName + " " + $scope.lastName;
+    //};
+});
+
+app.controller('searchCtrl', function($scope) {
+    $scope.searchParameter = "";
+	$scope.searchKeyword = "";
+});
