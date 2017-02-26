a6208d6f7e84b5bcdf110ae74ef92d8601dd72d1
Joshua <Joshua>
2017-02-26 12:00:45 -0600

    Updated DBTest with commenting for reference on how the servlet works.

--- WEB-INF/classes/DBTest.java
+++ WEB-INF/classes/DBTest.java
@@ -9,31 +9,33 @@ import javax.servlet.http.*;
 public class DBTest extends HttpServlet
 {
 
-    String foo = "Not Connected";
-    int bar = -1;
-	ResultSet rst;
+    String connectionStatus = "Not Connected";
+    //int bar = -1; //not used
+	ResultSet rst; //stores the restult of the database query
     
-    public void init()
+    public void init() //a class that would activate upon loading of the /DBTest URL --not needed
 	{
     }
 
 	public void doGet(HttpServletRequest request, //Tutorial from https://www.tutorialspoint.com/servlets/servlets-database-access.htm
 					  HttpServletResponse response)
 				throws ServletException, IOException
-	{
+	{ //this triggers when a GET call is made to the /DBTest URL
 	
 		// Set response content type
 		response.setContentType("text/html");
-	
+		
+		//Creating a response object so you just have to do out.println to sent <html> stuff
 		PrintWriter out = response.getWriter();
 		
 		//Formatting content and header for HTML page
 		String docType =
 			"<!doctype html public \"-//w3c//dtd html 4.0 " +
 			 "transitional//en\">\n";
-			
+		
+			//Build header of html
 			out.println(docType +
-			"<html>\n" +
+			"<html font-family=sans-serif >\n" +
 			"<head><title>" + "Database Result" + "</title></head>\n" + 
 			"<body bgcolor=\"#f0f0f0\">\n" + //Formatting
 			"<h1 align=\"center\">" + "Database Result:" + "</h1>\n"); //Formatting on top of page
@@ -47,30 +49,31 @@ public class DBTest extends HttpServlet
 
 		try // Documentation https://jdbc.postgresql.org/documentation/94/tomcat.html
         {
-            Context ctx = new InitialContext();
+            //attempt to connect to the database
+			Context ctx = new InitialContext();
             if(ctx == null )
                 throw new Exception("Boom - No Context");
     
             // /jdbc/postgres is the name of the resource above 
             DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/postgres");
         
-            if (ds != null) 
+            if (ds != null) //datasourse exists
             {
-                Connection conn = ds.getConnection();
+                Connection conn = ds.getConnection(); //gets connection from datasource
                 if(conn != null) 
                 {
-                    //foo = "Got Connection "+conn.toString();
+                    //connectionStatus = "Got Connection "+conn.toString();
                     Statement stmt = conn.createStatement(); 
-                    rst = stmt.executeQuery("select * from contrs.contacts"); //contains query results
+                    rst = stmt.executeQuery("select * from contrs."+(request.getParameter("SearchButton"))); //contains query results
                     ResultSetMetaData rstm = rst.getMetaData(); //Contains details like row count and column names for auto populating and creating table
-					int numOfCols = rstm.getColumnCount();
+					int numOfCols = rstm.getColumnCount(); //metadata from table to know how to build dynamically
 					
-					for ( int i=1; i<numOfCols+1; i++ ) {
+					for ( int i=1; i<numOfCols+1; i++ ) { //build top row of datase results table (formatting containg column names for reference)
 						out.print("<td><b>" + rstm.getColumnName(i) +
 						"</b></td>");
 					}
 					
-					while (rst.next()) {
+					while (rst.next()) { //while there are results, print them to the restults table.
 							out.print("<tr>");
 							for ( int i=1; i<numOfCols+1; i++ ) {
 									out.print("<td>" + rst.getString(i) + "</td>");
